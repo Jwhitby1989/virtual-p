ded37e89561413a6534427323604adc11151764b
Jwhitby <jw89@icloud.com>
2021-05-20 17:30:16 +0100

    isAlive/ Death Tests

--- Pet-Project/pet.js
+++ Pet-Project/pet.js
@@ -14,8 +14,11 @@ Pet.prototype = {
   }
   so if your fitness or age or hunger levels change then the getter function condition will potentiall change from true-false or vice versa
   */
-  isAlive() {
+  get isAlive() {
     this.isAlive = this.age < 30 && this.hunger < 10 && this.fitness > 0;
+    if (this.fitness < 0) {
+      return;
+    }
   },
   growUp() {
     this.age++;
@@ -38,17 +41,16 @@ Pet.prototype = {
   },
   // You might need to alter this - once your tests are correct it should give you an indication as to why and what you'll need to do - plus another check will need to be added to go along with the last few tests - check if the pet is alive before going on to do the rest of the check ups
   checkUp() {
-    if (this.fitness <= 3) {
-      return "I need a walk";
+    if (this.fitness <= 3 && this.hunger >= 5) {
+      return "I am hungry AND I need a walk";
     } else if (this.hunger >= 5) {
       return "I am hungry";
-    } else if (this.fitness <= 3 && this.hunger >= 5) {
-      return "I am hungry AND I need a walk";
+    } else if (this.fitness <= 3) {
+      return "I need a walk";
     } else {
       return "I feel great!";
     }
   },
-
 };
 
 module.exports = Pet;
